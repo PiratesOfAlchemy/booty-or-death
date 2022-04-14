79a1578f1ee21de06c881afbd75aa2934bcba044
Alex Blair <bclan11@gmail.com>
2022-04-14 15:58:44 -0700

    made one test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,4 +11,13 @@ describe('booty-or-death routes', () => {
   afterAll(() => {
     pool.end();
   });
+  it('should be able to get a prompt and 2 options', async () => {
+    const res = await request(app).get('/api/v1/prompts/');
+    const expected = await Model.getById(1);
+    expect(res.body).toEqual(expected);
+  });
+
+  it('should take user selection and return a new prompt with options', async () => {
+    const res = await request(app).get('/api/v1/prompts/').send();
+  });
 });
