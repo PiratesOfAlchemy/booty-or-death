de93f648d12b70603ac94b8f4d2e84d724708e7b
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-04-14 14:30:56 -0700

    setup sql table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,28 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS user_plot CASCADE;
+DROP TABLE IF EXISTS plot CASCADE;
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username TEXT NOT NULL
+);
+
+CREATE TABLE user_plot (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    block_id BIGINT REFERENCES plot(id),
+    user_id BIGINT REFERENCES users(id),
+    was_hero BOOLEAN REFERENCES plot(is_heroic)
+);
+
+CREATE TABLE plot (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    prompt TEXT NOT NULL,
+    heroic_choice TEXT NOT NULL,
+    villainous_choice TEXT NOT NULL,
+    heroic_block_id BIGINT,
+    is_heroic BOOLEAN NOT NULL,
+    villainous_block_id BIGINT
+);
