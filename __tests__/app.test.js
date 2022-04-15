eca907e26dfd49d7d4d07a95369ff35961f6dab4
Alex Blair <bclan11@gmail.com>
2022-04-14 17:49:59 -0700

    test for insert user name not hitting our post route correctly

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,7 +11,8 @@ describe('booty-or-death routes', () => {
   afterAll(() => {
     pool.end();
   });
-  it.only('should be able to get a prompt and two options by id', async () => {
+
+  it('should be able to get a prompt and two options by id', async () => {
     const res = await request(app).get('/api/v1/plots/1');
     const expected = {
       prompt: 'prompt 1',
@@ -20,4 +21,16 @@ describe('booty-or-death routes', () => {
     };
     expect(res.body).toEqual(expected);
   });
+
+  // it('should be able to insert a user name into users table', async () => {
+  //   const res = await request(app)
+  //     .post('/api/v1/users')
+  //     .send({ username: 'test' });
+
+  //   const expected = {
+  //     id: expect.any(String),
+  //     username: 'test',
+  //   };
+  //   expect(res.body).toEqual(expected);
+  // });
 });
